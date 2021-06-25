898c19de1a40d95e85b6ac7f5052bb901dde6b81
shanu3742 <krshanu.95@gmail.com>
2021-06-25 04:47:40 -0700

    first commit start here fryst commit databse is added

--- my-app/src/custom-hook.js
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react";
-
-function useModelState(defaultValue = false) {
-  const [isOpen, setIsOpen] = useState(defaultValue);
-  const open = useCallback(() => setIsOpen(true), []);
-  const close = useCallback(() => setIsOpen(false), []);
-  return { isOpen, open, close };
-}
-export default useModelState;
