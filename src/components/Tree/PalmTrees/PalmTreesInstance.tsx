8718266ea3b04d78a1b7f7449b4cb4d3ebb1ac2e
kumar shanu <kumar.shanu@stockedge.com>
2025-12-03 17:41:51 +0530

    tree json created

--- src/components/Tree/PalmTrees/PalmTreesInstance.tsx
+++ src/components/Tree/PalmTrees/PalmTreesInstance.tsx
@@ -1,20 +1,38 @@
 import React, { useMemo } from 'react'
 import { Instances, Instance, useGLTF } from '@react-three/drei'
+import * as d3 from 'd3'
+import palmTrees from '../../../setting/palmTree.json'
 
-const  PalmTreeInstance = ({count=100,scaleIndex=1}) =>  {
+
+
+const  PalmTreeInstance = ({scaleIndex=1}) =>  {
   const scale = useMemo(() =>[4*scaleIndex, 8*scaleIndex, 4*scaleIndex],[scaleIndex] )
-    const { nodes, materials } = useGLTF('/model/tree/PalmTree.gltf');
-    console.log('node',nodes);
+      const { nodes, materials } = useGLTF('/model/tree/PalmTree.gltf');
+      const zScale  = d3.scaleLinear().domain([0,1]).range([-2,-50])
+      const xScale  = d3.scaleLinear().domain([0,1]).range([-50,50])
+      
+      // tree position calculation
+      const trees = useMemo(() => {
+        return palmTrees.map((tree) => {
+          const x = xScale(tree.x)
+          const z = zScale(tree.z)
+        //   const rotationY = Math.random() * Math.PI * 2
+    
+          return {
+            position: [x, 2, z],
+          }
+        })
+      }, [])
+
+
     return (
       <Instances
-        limit={count}
+        limit={palmTrees.length}
         geometry={nodes.Environment_PalmTree_3.geometry}
         material={materials.Atlas}
       >
-        {Array.from({ length: count }).map((_, i) => {
-          const x = Math.random() * 100 - 50;
-          const z = Math.random() * -50;
-          return <Instance key={i} position={[x, 2, z]} scale={scale} />
+        {trees.map((tree, i) => {
+          return <Instance key={i} position={tree.position} scale={scale} />
         })}
       </Instances>
     );
